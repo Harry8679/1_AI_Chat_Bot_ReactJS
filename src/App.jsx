f3ddfa318dfd23729a5d5cae568f24a2d9e8074c
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-31 23:27:28 +0200

    isChatting State

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 // import './App.css'
+import { useState } from 'react'
 import ChatBotApp from './Components/ChatBotApp'
 // import ChatBotStart from './Components/ChatBotStart'
 
 const App = () => {
+  const [isChatting, setIsChatting] = useState(false);
 
   return (
     <div className='container'>
