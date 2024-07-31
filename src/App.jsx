ffe1588b8f5627b42a3649826917248d7f41b5c1
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-31 23:45:07 +0200

    Render the Component according the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import './App.css'
 import { useState } from 'react'
 import ChatBotApp from './Components/ChatBotApp'
+import ChatBotStart from './Components/ChatBotStart';
 // import ChatBotStart from './Components/ChatBotStart'
 
 const App = () => {
@@ -17,6 +18,11 @@ const App = () => {
   return (
     <div className='container'>
       {/* <ChatBotStart /> */}
+      {isChatting ? (
+        <ChatBotApp onGoBack={handleGoBack} />
+      ) : (
+        <ChatBotStart onStartChat={handleStartChat} />
+      )}
       <ChatBotApp />
     </div>
   )
