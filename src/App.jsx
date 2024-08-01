73f58751799118cd54dd529e5e4458fa65ca4f79
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-01 08:15:20 +0200

    Check if chats are empty or not

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,18 @@ import ChatBotStart from './Components/ChatBotStart';
 
 const App = () => {
   const [isChatting, setIsChatting] = useState(false);
+  const [chats, setChats] = useState([]);
 
   const handleStartChat = () => {
     setIsChatting(true);
+
+    if (chats.length === 0) {
+      const newChat = {
+        id: `Chat ${new Date().toLocaleDateString('en-GB')} ${new Date().toLocaleTimeString()}`,
+        messages: []
+      }
+      setChats([newChat]);
+    }
   }
 
   const handleGoBack = () => {
